e25003246e90267cdd034c31b236efadad10b005
Tal Bernard <123598057+tal-bernard@users.noreply.github.com>
2025-09-03 13:29:02 -0700

    updated ingredients schema

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,23 +1,35 @@
 import mongoose from 'mongoose';
 
+const ingredientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
 const recipeSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     trim: true,
   },
-  ingredients: {
-    type: [String],
-    required: true,
-  },
+  ingredients: [ingredientSchema],
   instructions: {
     type: String,
     required: true,
+    trim: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  prepTime: { type: Number },
+  cookTime: { type: Number },
+  numServings: { type: Number },
+  image: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
